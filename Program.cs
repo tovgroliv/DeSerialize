46b1773e4659af874ef2904b49327338f82fea59
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-03-19 16:10:12 +0300

    Добавлена сериализация списка

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 
 namespace DeSerialize
 {
@@ -42,15 +43,105 @@ namespace DeSerialize
 	{
 		public ListNode Head;
 		public ListNode Tail;
-		public int Count = 0;
+		public int Count;
 
 		public void Serialize(FileStream s)
 		{
+			ListNode activeHead = Head;
+			ListNode activeTail = Tail;
 
+			AddText(s, $"{Count}\n");
+
+			for (int i = 0; i < Count; i++)
+			{
+				ListNode active = activeTail;
+
+				if (i % 2 == 0)
+				{
+					active = activeHead;
+				}
+
+				string output = active.Data;
+
+				if (active.Rand == null)
+				{
+					output += "\n";
+				}
+				else
+				{
+					bool delta = true;
+					int n = 0;
+
+					FindDelta(ref delta, ref n, active);
+
+					output = $"{output} | {delta} | {n}\n";
+				}
+
+				AddText(s, output);
+
+				if (i % 2 == 0)
+				{
+					activeHead = active.Next;
+				}
+				else
+				{
+					activeTail = active.Prev;
+				}
+			}
 		}
 
 		public void Deserialize(FileStream s)
 		{
+			
+		}
+
+		private void FindDelta(ref bool delta, ref int n, ListNode active)
+		{
+			ListNode find = active.Rand;
+
+			ListNode activeHead = Head;
+			ListNode activeTail = Tail;
+			ListNode activeLocalHead = active;
+			ListNode activeLocalTail = active;
+
+			for (int i = 0; i < Count; i += 4)
+			{
+				if (find == activeHead)
+				{
+					delta = false;
+					n = i / 4;
+					return;
+				}
+				if (find == activeTail)
+				{
+					delta = false;
+					n = -i / 4;
+					return;
+				}
+				if (find == activeLocalHead)
+				{
+					delta = true;
+					n = i / 4;
+					return;
+				}
+				if (find == activeLocalTail)
+				{
+					delta = true;
+					n = -i / 4;
+					return;
+				}
+
+				activeHead = activeHead.Next;
+				activeTail = activeTail.Prev;
+				activeLocalHead = activeLocalHead.Next;
+				activeLocalTail = activeLocalTail.Prev;
+			}
+		}
+
+		private void AddText(FileStream fs, string value)
+		{
+			byte[] info = new UTF8Encoding(true).GetBytes(value);
+			fs.Write(info, 0, info.Length);
 		}
 	}
 
